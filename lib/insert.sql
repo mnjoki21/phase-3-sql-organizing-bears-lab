470d217d174ee94eff7ec41f983f3a4ad3f0c02a
mnjoki21 <mitchellenjoki7@gmail.com>
2022-08-23 23:48:18 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears 
+( name , age , sex , color , temparament , alive )
+VALUES 
+( 'Mr.Chocolate', 12 , 'M', 'yellow', 'melancholic' , true), 
+('Rowdy' , 23 , 'M', 'blue' , 'sanguine' , false), 
+('Tabitha' , 73 , 'F', 'purple' , 'sanguine' , true), 
+('Sergeant Brown' , 38 , 'M', 'blue' , 'sanguine' , false), 
+('Melissa' , 67 , 'F', 'blue' , 'sanguine' , true)
+('Grinch' , 56 , 'M', 'blue' , 'sanguine' , true)
+('Wendy' , 57 , 'F', 'blue' , 'sanguine' , true),
+(NULL , 57 , 'F', 'blue' , 'sanguine' , true);
